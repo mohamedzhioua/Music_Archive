427bce0af00cf577110cbc17a95cffada440d9ff
mohamedzhioua <zhiouamohamed1989@gmail.com>
2024-04-20 01:00:41 +0100

    commenting the login route that cuz a build error

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,38 +1,38 @@
-import dbConnect from "@/lib/dbConnect";
-import UserModel from "@/lib/models/UserModel";
-import bcrypt from "bcryptjs";
- import { NextRequest } from "next/server";
+// import dbConnect from "@/lib/dbConnect";
+// import UserModel from "@/lib/models/UserModel";
+// import bcrypt from "bcryptjs";
+//  import { NextRequest } from "next/server";
 
  
 
-export async function GET(req: NextRequest) {
-  await dbConnect();
+// export async function GET(req: NextRequest) {
+//   await dbConnect();
 
-  // Read data off req body
-  const body = await req.json();
-  const { name, password } = body;
+//   // Read data off req body
+//   const body = await req.json();
+//   const { name, password } = body;
 
-  // Validate data
-  if (!name || !password) {
-    return Response.json(
-      {
-        error: "Invalid name or password",
-      },
-      { status: 400 }
-    );
-  }
+//   // Validate data
+//   if (!name || !password) {
+//     return Response.json(
+//       {
+//         error: "Invalid name or password",
+//       },
+//       { status: 400 }
+//     );
+//   }
 
-  // Hash the password
-  const hash = bcrypt.hashSync(password, 8);
+//   // Hash the password
+//   const hash = bcrypt.hashSync(password, 8);
 
-  // Create a user in db
-  await UserModel.create({
-    name,
-    password: hash,
-    isAdmin: true,
-  });
+//   // Create a user in db
+//   await UserModel.create({
+//     name,
+//     password: hash,
+//     isAdmin: true,
+//   });
 
-  // return something
-  return Response.json({});
-}
+//   // return something
+//   return Response.json({});
+// }
  
